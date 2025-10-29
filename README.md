584edc6cbb66f31760592215ea727a35355d04af
wahl@gefak.de <wahl@gefak.de>
2025-10-29 11:43:31 +0100

    CORS Config für einfaches Development von SPA Clients - Nur im Dev Context zulässig

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # register-user-backend-dotnet
 Beispielrepository für Techinterviews
+
+## Systemvoraussetzungen
+- [.net 8](https://dotnet.microsoft.com/en-us/download/dotnet/8.0)
+
+## Anwendung bauen und starten
+
+Die befehle sind im gelichen Verzeichnis auszuführen in dem sich die .sln Datei befindet
+```powershell
+dotnet clean
+dotnet build
+dotnet run --project register-user-backend-dotnet\register-user-backend-dotnet.csproj
+```
+
+## wichtige URLs
+Frontend Platzhalter
+- https://localhost:5101/user
+
+OpenApi Dokument
+- https://localhost:5101/swagger/index.html
--- register-user-backend-dotnet/register-user-backend-dotnet/Program.cs
+++ register-user-backend-dotnet/register-user-backend-dotnet/Program.cs
@@ -19,6 +19,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddProblemDetails();
 builder.Services.AddFluentValidationRulesToSwagger();
 builder.Services.AddValidatorsFromAssemblyContaining<UserDtoValidator>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAnyOrigin",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 
 var app = builder.Build();
@@ -30,7 +40,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors("AllowAnyOrigin");
 app.UseSwagger();
 app.UseSwaggerUI();
 
